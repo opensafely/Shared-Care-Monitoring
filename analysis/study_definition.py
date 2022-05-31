3cd1b12ff5e050930372f6daedb36ac187102b00
Andrew Brown <75492467+a-d-brown@users.noreply.github.com>
2022-05-31 15:13:36 +0100

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,124 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import (
+    StudyDefinition,
+    codelist,
+    codelist_from_csv,
+    combine_codelists,
+    filter_codes_by_category,
+    patients,
+)
 
+from codelists import *
+
+start_date = "2019-09-01"
+end_date = "2021-07-01"
 
 study = StudyDefinition(
+    index_date=start_date,
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": start_date, "latest": end_date},
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+    population=patients.satisfying(
+        """
+       registered AND
+       NOT died AND
+       (age >=18 AND age <=120) AND 
+       (sex = 'M' OR sex = 'F') AND
+       (
+        (methotrexate_3months) OR
+        (leflunomide_3months) OR
+        (azathioprine_3months)
+       )
+       """
     ),
+    registered=patients.registered_as_of("index_date"),
+    died=patients.died_from_any_cause(
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1},
+    ),
+    
+    practice=patients.registered_practice_as_of(
+        "index_date",
+        returning="pseudo_id",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 25, "stddev": 5},
+            "incidence": 0.5,
+        },
+    ),
+    
+    age=patients.age_as_of(
+        "index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+    
+    age_band=patients.categorised_as(
+        {
+            "missing": "DEFAULT",
+            "0-19": """ age >= 0 AND age < 20""",
+            "20-29": """ age >=  20 AND age < 30""",
+            "30-39": """ age >=  30 AND age < 40""",
+            "40-49": """ age >=  40 AND age < 50""",
+            "50-59": """ age >=  50 AND age < 60""",
+            "60-69": """ age >=  60 AND age < 70""",
+            "70-79": """ age >=  70 AND age < 80""",
+            "80+": """ age >=  80 AND age <= 120""",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "missing": 0.005,
+                    "0-19": 0.125,
+                    "20-29": 0.125,
+                    "30-39": 0.125,
+                    "40-49": 0.125,
+                    "50-59": 0.125,
+                    "60-69": 0.125,
+                    "70-79": 0.125,
+                    "80+": 0.12,
+                }
+            },
+        },
+    ),
+    
+    practice_population=patients.satisfying(
+        """
+        age <=120 AND
+        registered
+        """
+    ),
+    
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.5, "F": 0.5}},
+        }
+    ),
+    
+    methotrexate_3months=patients.with_these_medications(
+        codelist=methotrexate_codelist,
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        between=["index_date - 3 months", "index_date"]
+    ),
+    
+    leflunomide_3months=patients.with_these_medications(
+        codelist=leflunomide_codelist,
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        between=["index_date - 3 months", "index_date"]
+    ),
+    
+    azathioprine_3months=patients.with_these_medications(
+        codelist=azathioprine_codelist,
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        between=["index_date - 3 months", "index_date"]
+    ),
+    
 )
